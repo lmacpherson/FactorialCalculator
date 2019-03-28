43454879737635cd03adc279b944d17a56e64205
lmacpherson <macpherson@student.ohlone.edu>
2019-03-28 12:46:22 -0700

    First commit

---
+++ FactorialCalculator/src/FactorialCalculator.java
@@ -0,0 +1,14 @@
+/**
+ * Author: Lauren MacPherson
+ * 
+ * Description: Use a for loop to calculate n!. Ask the user to enter an integer in the range 0 – 25, then display the result of the calculation.
+ *
+ **/
+import javax.swing.JOptionPane;
+
+public class FactorialCalculator {
+	//TODO: Initialize class variables
+	//TODO: Create get/set functions
+	//TODO: create for loop calculation function
+	
+}
---
+++ FactorialCalculator/src/FactorialCalculatorApp.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Lauren MacPherson
+ * 
+ * Description: Use a for loop to calculate n!. Ask the user to enter an integer in the range 0 – 25, then display the result of the calculation.
+ *
+ **/
+import javax.swing.JOptionPane;
+
+public class FactorialCalculatorApp {
+	
+	public static void main(String[] args) {
+		// TODO Create while loop to run program as long as user requests it
+
+	}
+
+}
